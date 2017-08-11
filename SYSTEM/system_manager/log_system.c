74976ee4902976b9499b59b08a5ce2a03a95372a
sunzhec2702 <sunzhe_c2702@163.com>
2017-08-11 19:35:09 +0800

    temp for home

--- SYSTEM/system_manager/log_system.c
+++ SYSTEM/system_manager/log_system.c
@@ -3,22 +3,17 @@
 #include "ff.h"
 #include "string.h"
 
-#define ERROR_FILE "error.log" // error.log. For end user analysis.
-
-#define LOG_FOLDER "0:/logs" // 0 is sd card.
-
-#define MAX_FILE_NAME_LEN 20
-#define MAX_LOG_MESSAGE_LEN 30
-
 static DIR log_dir;
-static FIL log_file, record_file;
+static FIL log_file, daily_record_file, total_record_file, error_file;
+
 static char log_file_name[MAX_FILE_NAME_LEN];
-static char record_file_name[MAX_FILE_NAME_LEN];
+static char daily_record_file_name[MAX_FILE_NAME_LEN];
+static char total_record_file_name[MAX_FILE_NAME_LEN];
+static char error_file_name[MAX_FILE_NAME_LEN];
 
 static char log_message[MAX_LOG_MESSAGE_LEN];
-                                                                      static char error_message[MAX_LOG_MESSAGE_LEN];
+static char error_message[MAX_LOG_MESSAGE_LEN];
 
-static calendar_obj output_start;
 
 /* Maybe we have to make sure the sd card is formatted or not */
 /* 
@@ -34,6 +29,8 @@ ErrorStatus log_system_init()
     if (is_calendar_initialized() == FALSE)
         return ERROR;
     u32 ret = FR_OK;
+
+    // Create the log folder if not existed.
     ret = f_opendir(&log_dir, LOG_FOLDER);
     if (ret == FR_NO_PATH || ret == FR_NO_FILE)
     {
@@ -42,28 +39,54 @@ ErrorStatus log_system_init()
         {
             return ERROR;
         }
+        ret = f_opendir(&log_dir, LOG_FOLDER);
+        if (ret != FR_OK)
+            return ERROR;
     }
-    ret = f_opendir(&log_dir, LOG_FOLDER);
-    if (ret != FR_OK)
-        return ERROR;
+    f_closedir(&log_dir);
 
+    // Initiate the three file names.
     // Get the current calendar.
     calendar_obj *cur_cal = get_current_calendar();
     snprintf(log_file_name, MAX_FILE_NAME_LEN, "%s/%d_%d_%d.log", LOG_FOLDER, cur_cal->year, cur_cal->month, cur_cal->date);
-    snprintf(record_file_name, MAX_FILE_NAME_LEN, "%s/%d_%d_%d.dat", LOG_FOLDER, cur_cal->year, cur_cal->month, cur_cal->date);
-    // Try to create a new file
+    snprintf(daily_record_file_name, MAX_FILE_NAME_LEN, "%s/%d_%d_%d.dat", LOG_FOLDER, cur_cal->year, cur_cal->month, cur_cal->date);
+    snprintf(total_record_file_name£¬ MAX_FILE_NAME_LEN, "%s/record_summary.dat", LOG_FOLDER);
+    snprintf(error_file_name, MAX_FILE_NAME_LEN, "%s/error_log.txt", LOG_FOLDER);
+
+    // Log File
     ret = f_open(&log_file, log_file_name, FA_READ | FA_WRITE | FA_OPEN_ALWAYS);
     if (ret != FR_OK)
         return ERROR;
     f_sync(&log_file);
-    f_close(&log_file);
-    ret = f_open(&record_file, record_file_name, FA_READ | FA_WRITE | FA_OPEN_ALWAYS);
+    //f_close(&log_file);
+    // Daily record file
+    ret = f_open(&daily_record_file, daily_record_file_name, FA_READ | FA_WRITE | FA_OPEN_ALWAYS);
     if (ret != FR_OK)
         return ERROR;
-    f_sync(&record_file);
-    f_close(&record_file);
+    f_sync(&daily_record_file);
+    //f_close(&daily_record_file);
+    // Total record file
+    ret = f_open(&total_record_file, total_record_file_name, FA_READ | FA_WRITE | FA_OPEN_ALWAYS);
+    if (ret != FR_OK)
+        return ERROR;
+    f_sync(&total_record_file);
+    //f_close(&total_record_file);
+    // Error file
+    ret = f_open(&error_file, error_file_name, FA_READ | FA_WRITE | FA_OPEN_ALWAYS);
+    if (ret != FR_OK)
+        return ERROR;
+    f_sync(&error_file);
+
     update_output_log();
     return SUCCESS;
+}
+
+void close_all_files()
+{
+    f_close(&log_file);
+    f_close(&daily_record_file);
+    f_close(&total_record_file);
+    f_close(&error_file);
 }
 
 // TODO: We should print the machine status as well.
@@ -72,25 +95,20 @@ ErrorStatus update_output_log()
     u8 ret = 0;
      if (is_calendar_initialized() == FALSE)
         return ERROR;
-    u32 byte_write;
-    memset(log_message, 0, MAX_LOG_MESSAGE_LEN);
     calendar_obj *cur_cal = get_current_calendar();
-    // print a log.
-    snprintf(log_message, MAX_LOG_MESSAGE_LEN, "%d-%d-%d-%d-%d-%d\n", cur_cal->year, cur_cal->month, cur_cal->date, cur_cal->hour, cur_cal->min, cur_cal->sec);
-    if (f_open(&log_file, log_file_name, FA_READ | FA_WRITE | FA_OPEN_EXISTING) != FR_OK)
+    if (f_lseek(&log_file, log_file.fsize) != FR_OK)
         return ERROR;
-    ret = f_lseek(&log_file, log_file.fsize);
-    if (ret != FR_OK)
+    if (f_printf(&log_file, "%d-%d-%d-%d-%d-%d\n", cur_cal->year, cur_cal->month, cur_cal->date, cur_cal->hour, cur_cal->min, cur_cal->sec) != FR_OK)
         return ERROR;
-    ret = f_write(&log_file, log_message, strlen(log_message), &byte_write);
-    if (ret != FR_OK)
+    if (f_sync(&log_file) != FR_OK)
         return ERROR;
-    f_sync(&log_file);
-    f_close(&log_file);
-    // TODO: update the accumulate time.
-    // read current time, compare with the start time.
-    // snprintf()
+    //f_close(&log_file);
     return SUCCESS;
+}
+
+ErrorStatus update_record_log()
+{
+    
 }
 
 void print_debug_log(char *message)
--- SYSTEM/system_manager/log_system.h
+++ SYSTEM/system_manager/log_system.h
@@ -3,14 +3,39 @@
 #include "sys.h"
 #include "rtc.h"
 
+#define ERROR_FILE "error.log" // error.log. For end user analysis.
+#define LOG_FOLDER "0:/logs" // 0 is sd card.
+
+#define MAX_FILE_NAME_LEN 30
+#define MAX_LOG_MESSAGE_LEN 30
 
 /* We should update the use time by adding some delta time.
 Once we power on the system, the enable flag is false until the button is pressed.
 Then update the start time using RTC.
 Check today's log file existance. If existed, open and update. two file, one for log, one for summary.
-
 TODO: log more often. Summary to variable and update rarely.
 */
+
+typedef struct
+{
+    u32 start_time;
+    u32 end_time;
+} single_use_time_t; 
+
+typedef struct
+{
+    u32 total_time;
+    calendar_obj date_info;
+    u32 use_count;
+    single_use_time_t single_use[0];
+} daily_use_time_t;
+
+typedef struct
+{
+    u32 hour;
+    u32 min;
+    u32 sec;
+} total_use_time_t;
 
 /* Compare current time and start time to update the daily_use_time and total_use_time */
 
@@ -19,6 +44,7 @@ extern void update_total_use_time(void);
 
 // Create the log file according to the current date.
 extern ErrorStatus log_system_init(void);
+extern void close_all_files(void);
 extern ErrorStatus update_output_log(void);
 extern void print_debug_log(char *message);
 
